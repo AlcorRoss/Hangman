9527baee109ce03eecb9dea506fb411076b90d6d
AlcorRoss <sokolovdenic1@mail.ru>
2022-12-08 21:38:23 +0300

    -

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,2 +1,18 @@
 public class Gameplay {
+
+    public static void gameplay() {
+        int counter = 0;
+        String word = Dictionary.wordChoice();
+
+        while (counter <= 6) {
+
+            if(counter==6){
+                System.out.println("Ты проиграл!");
+            }
+
+            System.out.println("Допущено ошибок: " + counter);
+
+        }
+
+    }
 }
